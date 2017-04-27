628aa7778fe898d6db528429cb140a01d1a477cd
benrau87 <benrau87@users.noreply.github.com>
2017-04-27 10:38:03 -0500

    Update config_64-bitVM.sh

--- config_64-bitVM.sh
+++ config_64-bitVM.sh
@@ -3,3 +3,12 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
+echo -e "${YELLOW}What VM would you like to create antivm scripts for?${NC}"
+read name
+python antivmdetect.py
+mkdir $name/
+cp DSTS-Intel* $name/DSTS_VMwareVirtualPlatform.bin
+mv VMwareVirtualPlatform.sh $name/
+mv VMwareVirtualPlatform.ps1 $name/
+cd $name/
+sed -i 's/"$1"/"$name/g' VMwareVirtualPlatform.sh

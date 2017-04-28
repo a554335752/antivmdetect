192a8278a8327d852731b2c33d029d6ddff3563b
benrau87 <benrau87@users.noreply.github.com>
2017-04-28 10:19:14 -0500

    Create config_example.sh

---
+++ config_example.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo -e "${YELLOW}What VM would you like to create antivm scripts for?${NC}"
+read name
+mkdir $name/
+cp DSDT-Intel* $name/DSDT-Intel*
+mv eample $name/virtualboxsetup.sh
+chmod +x virtualboxsetup.sh
+#!/bin/bash
+
+RANGE=255
+number=$RANDOM
+numbera=$RANDOM
+numberb=$RANDOM
+let "number %= $RANGE"
+let "numbera %= $RANGE"
+let "numberb %= $RANGE"
+octets='00-19-eC'
+octeta=`echo "obase=16;$number" | bc`
+octetb=`echo "obase=16;$numbera" | bc`
+octetc=`echo "obase=16;$numberb" | bc`
+macadd="${octets}-${octeta}-${octetb}-${octetc}"
+
+cd $name/
+sed -i 's/"$1"/'"$name"'/g' virtualboxsetup.sh
+sed -i 's/0015C5000001/'"$macadd"'/g' virtualboxsetup.sh
+

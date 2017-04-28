970a68e272987732d94e4893d0dea4ceb64408e9
benrau87 <benrau87@users.noreply.github.com>
2017-04-28 10:16:17 -0500

    Update config_32-bitVM.sh

--- config_32-bitVM.sh
+++ config_32-bitVM.sh
@@ -5,11 +5,26 @@ if [ "$EUID" -ne 0 ]
 fi
 echo -e "${YELLOW}What VM would you like to create antivm scripts for?${NC}"
 read name
-python antivmdetect.py
 mkdir $name/
-cp DSDT-Intel* $name/DSDT_VMwareVirtualPlatform.bin
-mv VMwareVirtualPlatform.sh $name/
-mv VMwareVirtualPlatform.ps1 $name/
+cp DSDT-Intel* $name/DSDT-Intel*
+mv eample $name/virtualboxsetup.sh
+chmod +x virtualboxsetup.sh
+#!/bin/bash
+
+RANGE=255
+number=$RANDOM
+numbera=$RANDOM
+numberb=$RANDOM
+let "number %= $RANGE"
+let "numbera %= $RANGE"
+let "numberb %= $RANGE"
+octets='00-19-eC'
+octeta=`echo "obase=16;$number" | bc`
+octetb=`echo "obase=16;$numbera" | bc`
+octetc=`echo "obase=16;$numberb" | bc`
+macadd="${octets}-${octeta}-${octetb}-${octetc}"
+
 cd $name/
-sed -i 's/"$1"/'"$name"'/g' VMwareVirtualPlatform.sh
+sed -i 's/"$1"/'"$name"'/g' virtualboxsetup.sh
+sed -i 's/"$1"/'"$name"'/g' virtualboxsetup.sh
 

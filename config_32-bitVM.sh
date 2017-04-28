0742667400c44aa25c9dab7c7291b1bd6fb03bf5
benrau87 <benrau87@users.noreply.github.com>
2017-04-28 10:18:19 -0500

    Update config_32-bitVM.sh

--- config_32-bitVM.sh
+++ config_32-bitVM.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit 1
-fi
 echo -e "${YELLOW}What VM would you like to create antivm scripts for?${NC}"
 read name
 mkdir $name/

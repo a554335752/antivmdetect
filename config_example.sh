15891daf8d11c0e78cb3aa43c73babfe23e35ee3
benrau87 <benrau87@users.noreply.github.com>
2017-04-28 14:16:26 -0500

    Update config_example.sh

--- config_example.sh
+++ config_example.sh
@@ -4,7 +4,7 @@ read name
 mkdir $name/
 cp DevManView.exe $name/
 cp Volumeid.exe $name/
-cp DSDT* $/name
+cp DSDT* $name/
 cp example.ps1 $name/guestsetup.ps1
 cp example.sh $name/virtualboxsetup.sh
 touch $name/computer.lst

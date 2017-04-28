b294970b3aadc05f3d229c2671d833bfc687e335
benrau87 <benrau87@users.noreply.github.com>
2017-04-28 10:20:17 -0500

    Update config_32-bitVM.sh

--- config_32-bitVM.sh
+++ config_32-bitVM.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
 echo -e "${YELLOW}What VM would you like to create antivm scripts for?${NC}"
 read name
+python antivmdetect.py
 mkdir $name/
-cp DSDT-Intel* $name/DSDT-Intel*
-mv eample $name/virtualboxsetup.sh
-chmod +x virtualboxsetup.sh
-#!/bin/bash
-
-RANGE=255
-number=$RANDOM
-numbera=$RANDOM
-numberb=$RANDOM
-let "number %= $RANGE"
-let "numbera %= $RANGE"
-let "numberb %= $RANGE"
-octets='00-19-eC'
-octeta=`echo "obase=16;$number" | bc`
-octetb=`echo "obase=16;$numbera" | bc`
-octetc=`echo "obase=16;$numberb" | bc`
-macadd="${octets}-${octeta}-${octetb}-${octetc}"
-
+cp DSDT-Intel* $name/DSDT_VMwareVirtualPlatform.bin
+mv VMwareVirtualPlatform.sh $name/
+mv VMwareVirtualPlatform.ps1 $name/
 cd $name/
-sed -i 's/"$1"/'"$name"'/g' virtualboxsetup.sh
-sed -i 's/0015C5000001/'"$macadd"'/g' virtualboxsetup.sh
-
+sed -i 's/"$1"/'"$name"'/g' VMwareVirtualPlatform.sh

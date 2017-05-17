cede514ebb47b8d5a123ddf762eb015449be108d
benrau87 <benrau87@users.noreply.github.com>
2017-05-17 12:06:31 -0500

    Update example_64-bit.ps1

--- example_64-bit.ps1
+++ example_64-bit.ps1
@@ -1,3 +1,8 @@
+reg add "hklm\software\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultUserName /d <USERNAME> /t REG_SZ /f
+reg add "hklm\software\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultPassword /d <PASSWORD> /t REG_SZ /f
+reg add "hklm\software\Microsoft\Windows NT\CurrentVersion\WinLogon" /v AutoAdminLogon /d 1 /t REG_SZ /f
+reg add "hklm\system\CurrentControlSet\Control\TerminalServer" /v AllowRemoteRPC /d 0x01 /t REG_DWORD /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /d 0x01 /t REG_DWORD /f
 $version = (Get-WmiObject win32_operatingsystem).version
 Copy-Item -Path HKLM:\HARDWARE\ACPI\DSDT\VBOX__ -Destination HKLM:\HARDWARE\ACPI\DSDT\HP -Recurse
 Remove-Item -Path HKLM:\HARDWARE\ACPI\DSDT\VBOX__ -Recurse
